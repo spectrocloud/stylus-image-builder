d3dd55b5c9e9e8ae7fe9aa22c310751fe38a01dc
Luther Monson <luther.monson@gmail.com>
2022-12-12 10:20:01 -0700

    raw

--- builder/scripts/post_vmdk.sh
+++ builder/scripts/post_vmdk.sh
@@ -2,6 +2,6 @@
 
 echo 'Converting raw to vmdk'
 
-qemu-img convert -p -f qcow2 -O vmdk -o adapter_type=lsilogic,subformat=streamOptimized,compat6 \
+qemu-img convert -p -f raw -O vmdk -o adapter_type=lsilogic,subformat=streamOptimized,compat6 \
   $CWD/images/stylus-image-builder \
   $CWD/images/stylus-image-builder.vmdk
