4aaf7a5449370c9768284453c9a52868ad17fc51
Rishi <rishi.anand0@gmail.com>
2023-10-20 15:37:10 +0530

    Making user-data as optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,33 +5,34 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-if [ -z "$PALETTE_ENDPOINT" ]; then
-  echo "Environment Variable PALETTE_ENDPOINT must be set"
-  exit 1
-fi
-
-if [[ -z "$REGISTRATION_URL" && -z $EDGE_HOST_TOKEN ]]; then
-  echo "Environment Variable REGISTRATION_URL or EDGE_HOST_TOKEN must be set"
-  exit 1
-fi
-
 if [ -z "$ISO_URL" ]; then
   echo "Environment Variable ISO_URL must be set"
   exit 1
 fi
 
-echo "Writing user-data file"
-yq e -i '.stylus.site.paletteEndpoint = env(PALETTE_ENDPOINT)' user-data
-if [ ! -z "$REGISTRATION_URL" ]; then
-  yq e -i '.stylus.site.registrationURL = env(REGISTRATION_URL)' user-data
-fi
-if [ ! -z "$EDGE_HOST_TOKEN" ]; then
-  yq e -i '.stylus.site.edgeHostToken = env(EDGE_HOST_TOKEN)' user-data
-fi
+if [ -n "$PALETTE_ENDPOINT" ]; then
+  echo "PALETTE_ENDPOINT is set to: $PALETTE_ENDPOINT"
 
-echo "----------------------USERDATA----------------------"
-cat user-data
-echo "----------------------USERDATA----------------------"
+  if [ -z "$EDGE_HOST_TOKEN" ]; then
+    echo "Environment Variable EDGE_HOST_TOKEN must be set"
+    exit 1
+  fi
+
+  mv user-data.template user-data
+  mv meta-data.template meta-data
+  echo "Writing user-data file"
+  yq e -i '.stylus.site.paletteEndpoint = env(PALETTE_ENDPOINT)' user-data
+  if [ ! -z "$REGISTRATION_URL" ]; then
+    yq e -i '.stylus.site.registrationURL = env(REGISTRATION_URL)' user-data
+  fi
+  if [ ! -z "$EDGE_HOST_TOKEN" ]; then
+    yq e -i '.stylus.site.edgeHostToken = env(EDGE_HOST_TOKEN)' user-data
+  fi
+
+  echo "----------------------USERDATA----------------------"
+  cat user-data
+  echo "----------------------USERDATA----------------------"
+fi
 
 # ISO should be in /stylus-image-builder/file.iso and can be mounted in or auto-downloaded if not embedded in image
 isoFile=$(basename $ISO_URL)
